e00f975946728d91f8d0547fc5de23166d6c7995
fahali <fahim.ali.204@gmail.com>
2021-01-13 14:56:06 -0500

    Added React Bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Header from './components/Header';
 
-function App() {
+const App = () => {
    return (
       <div className='App'>
          <Header />
       </div>
    );
-}
+};
 
 export default App;
