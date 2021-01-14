60fd42d0f888b8cb78a1796a53f66635570e23d3
fahali <fahim.ali.204@gmail.com>
2021-01-13 22:31:35 -0500

    Good styling on cards.

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,4 +1,5 @@
 import Card from 'react-bootstrap/Card';
+import moment from 'moment';
 
 const Movie = ({ movie }) => {
    const capitalizeName = name =>
@@ -7,18 +8,24 @@ const Movie = ({ movie }) => {
          .map(char => char.toUpperCase())
          .join('');
 
+   const friendlyDate = date =>
+      moment(date, 'YYYY-MM-DD').format('MMMM D, YYYY');
+
    return (
-      <Card>
-         <Card.Img
-            variant='top'
-            src={movie.multimedia && movie.multimedia.src}
-         />
+      <Card bg='secondary' border='primary'>
+         <Card.Header>
+            <small>{friendlyDate(movie.publication_date)}</small>
+         </Card.Header>
          <Card.Body>
             <Card.Title>{movie.display_title}</Card.Title>
-            <Card.Subtitle className='mb-2 text-muted'>
+            <Card.Subtitle className='mb-2'>
                {capitalizeName(movie.byline)}
             </Card.Subtitle>
          </Card.Body>
+         <Card.Img
+            variant='bottom'
+            src={movie.multimedia && movie.multimedia.src}
+         />
       </Card>
    );
 };
