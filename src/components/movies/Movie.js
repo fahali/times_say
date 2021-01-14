8a8c853ec82092f92bda8ed4142e49a3705f527d
fahali <fahim.ali.204@gmail.com>
2021-01-13 21:26:59 -0500

    Cards are taking shape.

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,10 +1,23 @@
 import Card from 'react-bootstrap/Card';
 
 const Movie = ({ movie }) => {
+   const capitalizeName = name =>
+      name
+         .split('')
+         .map(char => char.toUpperCase())
+         .join('');
+
    return (
       <Card>
+         <Card.Img
+            variant='top'
+            src={movie.multimedia && movie.multimedia.src}
+         />
          <Card.Body>
             <Card.Title>{movie.display_title}</Card.Title>
+            <Card.Subtitle className='mb-2 text-muted'>
+               {capitalizeName(movie.byline)}
+            </Card.Subtitle>
          </Card.Body>
       </Card>
    );
