1557ea76eb92bdb80132d3c8d88a33ba298b0f76
fahali <fahim.ali.204@gmail.com>
2021-01-14 21:24:45 -0500

    Fixed annoying bug with missing info from API.

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -2,14 +2,27 @@ import Card from 'react-bootstrap/Card';
 import { capitalizeName, friendlyDate } from './helper/Helper';
 
 const MovieDetails = ({ movie }) => {
+   const headerDetails = () => {
+      let details = '';
+
+      if (movie.opening_date && movie.mpaa_rating) {
+         details = `${friendlyDate(movie.opening_date)} | Rated ${
+            movie.mpaa_rating
+         }`;
+      } else if (movie.opening_date) {
+         details = friendlyDate(movie.opening_date);
+      } else if (movie.mpaa_rating) {
+         details = `Rated ${movie.mpaa_rating}`;
+      }
+
+      return details;
+   };
+
    return (
       <Card bg='dark' className='details-card'>
          <Card.Header className='details-header'>
             <h1>{movie.display_title}</h1>
-            <p>
-               {movie.opening_date && friendlyDate(movie.opening_date)}
-               {movie.mpaa_rating && ` | Rated ${movie.mpaa_rating}`}
-            </p>
+            <p>{headerDetails()}</p>
          </Card.Header>
          <Card.Body className='details-body'>
             <Card.Title>{movie.headline}</Card.Title>
